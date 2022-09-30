7a8bca7ded7937a3b9a885de43b072e4a3cb75a7
Tina Huynh <tinamchuynh@gmail.com>
2022-09-30 11:02:31 -0700

    to Hex

--- converter.js
+++ converter.js
@@ -31,3 +31,31 @@ function toRgb() {
         invalid(hexInput, rgbInput);
     }
 }
+
+function toHex() {
+    let rgbCode = rgbInput.value;
+    let rgbRegex1 = /^rgb\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)$/;
+    let rgbRegex2 = /^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}$/
+    let hex = "#";
+    if (rgbRegex1.test(rgbCode) || rgbRegex2.test(rgbCode)) {
+        rgbCode = rgbCode.replace(/[rgb()]+/g, "") || rgbCode;
+        rgbCode = rgbCode.split(",");
+        let condition = rgbCode.every((value) => {
+            return parseInt(value) <= 255;
+        });
+        if (condition) {
+            valid(rgbInput);
+            rgbCode.forEach(value => {
+                value = parseInt(value).toString(16);
+                hex += value.length == 1 ? "0" + value : value;
+            });
+            hexInput.value = hex;
+            document.body.style.backgroundColor = hex;
+        } else {
+            invalid(rgbInput, hexInput);
+        }
+    } else {
+        invalid(rgbInput, hexInput);
+    }
+
+}
